4c220a9b832735f1c1ef75a5685e33e474956001
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm8350-common: Setup DeviceAsWebcam
    Change-Id: I75c3142c9e175c4b207eade4926777b717d4d9e8
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- common.mk
+++ common.mk
@@ -232,6 +232,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    DeviceAsWebcamResTarget \
     FrameworksResTarget \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
