695702da5a154b9df1635c2bbe59e5e9d735bb82
Arian <arian.kulmer@web.de>
2026-01-26 10:10:14 +0100

    sm8350-common: Import qcom perf boosts into powerhint.json
    Generated using https://github.com/ArianK16a/qcom-perf-parser
    
    Change-Id: Ic43199fa3aef3e5276e31e83c14ecefdf2136c7a
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- common.mk
+++ common.mk
@@ -250,19 +250,21 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
-    
+
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI        # For AOSP
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Sensors
 PRODUCT_PACKAGES += \
