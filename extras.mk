0bb822261c2effa77376f6f8b38ee37dcad434fa
Jammy555 <prathap.venkata@gmail.com>
2026-03-08 18:36:03 +0530

    DeviceSettings: (SQUASH) Switch to Material3E design
    **Introduce Powertools 3.0
    **Introduce Thermal limiter
    
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- extras.mk
+++ extras.mk
@@ -13,8 +13,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.iorapd.enable=false \
     iorapd.perfetto.enable=false \
     persist.sys.perf.scroll_opt=true \
-    persist.sys.perf.scroll_opt.heavy_app=1
-
+    persist.sys.perf.scroll_opt.heavy_app=1 
+    
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.post_boot.parsed=1
 
