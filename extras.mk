bc06f2670901a0760a66fe3f350770eb978c3f16
abhinav2005git <abhinavanand.xda@gmail.com>
2026-05-02 08:18:16 +0000

    init: QTI PowerHAL: Failed to acquire lock...
    Change-Id: I7a7460c29337a94c3f25e48de886dcefa4f486fe
    Signed-off-by: adiatul16 <adiatul16@gmail.com>
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- extras.mk
+++ extras.mk
@@ -4,5 +4,9 @@ TARGET_BUILD_DEVICE_AS_WEBCAM := true
 # OnePlus OOS Camera
 #$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
-# Dolby 
-#$(call inherit-product, hardware/oplus/dolby/dolby.mk)
+# powerhal properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    pm.sleep_mode=1
+
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.post_boot.parsed=1
